41e592d362595ddc0ad7b2fbbe5a0527ac210a64
Aecio Batista <aecio.batista@hotmail.com>
2024-09-15 18:55:31 -0300

    Aula 01 - Desafio

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+CONSTANTE_BONUS = 1000
+
+nome_usuario = input("Digite o seu nome: ")
+
+salaraio_usuario = float(input("Digite o seu salario: "))
+
+bonus_usuario = float(input("Digite o seu bonus: "))     
+
+valor_bonus = CONSTANTE_BONUS + salaraio_usuario * bonus_usuario
+
+print(f"O usuário {nome_usuario} possui o bonus de {valor_bonus}")
